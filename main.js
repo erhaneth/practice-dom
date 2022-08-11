65bc3fb9d87d011df470fab95ce80c9d4910dd78
“erhaneth” <gumusucb21@gmail.com>
2022-08-10 23:26:35 -0700

    First Commit

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 // practice with javascript
+console.log('Hello World');
+
+document.addEventListener('DOMContentLoaded', function() {
+    //  Select the element with the id of 'button-submit'
+    document.querySelector('#button-submit').addEventListener('click', function() {
+        console.log('BUTTON CLICKED');
+    });  
+    //select the element with the id of 'button-clear'
+    document.querySelector('#button-clear').addEventListener('click', function() {
+        console.log('BUTTON CLEARED');
+    });
+
+
+
+
+
+
+});
